c75aa17a2449c56f8380e193c50a078110f8d19f
Alan Cavalcante <61800828+alan589@users.noreply.github.com>
2025-03-11 15:56:01 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+import csv
+import datetime as dt
+import smtplib
+import random
+import os
+
+today = dt.datetime.now()
+
+MY_EMAIL = ""
+MY_PASS = ""
+SMTP = ""
+
+with open("birthdays.csv", "r") as birthday_file:
+    birthdays = csv.DictReader(birthday_file)
+    for birthday in birthdays:
+        birthday_month = int(birthday["month"])
+        birthday_day = int(birthday["day"])
+        if today.month ==  birthday_month and today.day == birthday_day:
+            path = "./letter_templates/"
+            files = os.listdir(path)
+            random_letter = random.choice(files)
+            with open(os.path.join(path + random_letter), "r") as letter_file:
+                letter = ''.join(letter_file.readlines())
+                birthday_name = birthday["name"]
+                letter = letter.replace("[NAME]", birthday_name)
+
+            with smtplib.SMTP(SMTP) as connection:
+                connection.starttls()
+                connection.login(MY_EMAIL, MY_PASS)
+                birthday_email = birthday["email"]
+                connection.sendmail(
+                    from_addr=MY_EMAIL,
+                    to_addrs=birthday_email,
+                    msg=f"Subject:Happy Birthday!!!\n\n{letter}"
+                )
+                print("Email sent!!!")
+
+
+
+
